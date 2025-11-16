bcfae3fedecc8d6132c61d99e5fb9e68bbd19486
kkhhjjoo <khjoo1203@gmail.com>
2025-11-16 18:53:36 +0900

    feature/utils axios추가

--- types/upload.ts
+++ types/upload.ts
@@ -1,4 +1,4 @@
-import { api } from './apiClient';
+import { getAxios } from '../src/features/utils/axios';
 
 interface UploadItem {
   path?: string;
@@ -15,9 +15,14 @@ export async function uploadImage(file: File): Promise<string> {
   // 서버 요구사항: 파일 필드는 'attach'
   formData.append('attach', file);
 
-  const { data } = await api.post<UploadResponse>('/files/', formData, {
-    headers: { 'Content-Type': 'multipart/form-data' },
-  });
+  const axiosInstance = getAxios();
+  const { data } = await axiosInstance.post<UploadResponse>(
+    '/files/',
+    formData,
+    {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    },
+  );
 
   if (data?.url) return data.url;
   if (data?.item && data.item.length > 0) {
