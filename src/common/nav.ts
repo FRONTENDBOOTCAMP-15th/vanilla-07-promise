ce94f7a48e8d98c28f5d9cbb40cd9154156185b2
kimeunjae-11 <kejkej1103@gmail.com>
2025-11-17 15:24:05 +0900

    fix: 이미지 오류

--- src/common/nav.ts
+++ src/common/nav.ts
@@ -1,3 +1,8 @@
+import homeImage from '/assets/images/home.svg';
+import searchImage from '/assets/images/nav.search.svg';
+import writeImage from '/assets/images/write.svg';
+import myboxImage from '/assets/images/mybox.svg';
+
 class BrunchNav extends HTMLElement {
   connectedCallback() {
     this.render();
@@ -5,26 +10,26 @@ class BrunchNav extends HTMLElement {
 
   render() {
     this.innerHTML = `
-      <link rel="stylesheet" href="/assets/components/nav.css" />
+     
       <nav class="nav">
       <!-- 홈 페이지 html넣기-->
         <a href="home.html" class="nav-icon" data-name="home">
-          <img src="/assets/images/home.svg" alt="홈" />
+          <img src="${homeImage}" alt="홈" />
           <span>홈</span>
         </a>
       <!-- 발견 페이지 html넣기-->
         <a href="search.html" class="nav-icon" data-name="search">
-          <img src="/assets/images/nav.search.svg" alt="발견" />
+          <img src="${searchImage}" alt="발견" />
           <span>발견</span>
         </a>
       <!--글쓰기 페이지 html넣기-->
         <a href="write.html" class="nav-icon" data-name="write">
-          <img src="/assets/images/write.svg" alt="글쓰기" />
+          <img src="${writeImage}" alt="글쓰기" />
           <span>글쓰기</span>
         </a>
         <!--내 서랍 페이지 html넣기-->
         <a href="mybox.html" class="nav-icon" data-name="mybox">
-          <img src="/assets/images/mybox.svg" alt="내 서랍" />
+          <img src="${myboxImage}" alt="내 서랍" />
           <span>내 서랍</span>
         </a>
       </nav>
